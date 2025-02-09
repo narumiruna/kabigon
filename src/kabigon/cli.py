3942dd5ceef59a65614c5def7831a22d2830b18b
narumi <toucans-cutouts0f@icloud.com>
2025-02-10 04:15:47 +0800

    ✨ feat(cli): implement CLI command for URL loading with rich output ➕ add click and rich dependencies

--- src/kabigon/cli.py
+++ src/kabigon/cli.py
@@ -1,2 +1,11 @@
-def main():
-    print("Hello, world!")
+import click
+from rich import print
+
+from .pipeline import PipelineLoader
+
+
+@click.command()
+@click.argument("url", type=click.STRING)
+def main(url: str) -> None:
+    loader = PipelineLoader().load(url)
+    print(loader)
