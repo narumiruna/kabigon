f1ded342fcbd3b043ea6ddcee5e7872d92c0077d
narumi <toucans-cutouts0f@icloud.com>
2025-03-10 19:32:55 +0800

    ✨ feat(loader): add async_load method for asynchronous loading

--- src/kabigon/compose.py
+++ src/kabigon/compose.py
@@ -50,3 +50,22 @@ class Compose(Loader):
                 logger.info("[{}] Failed to load URL: {}, got error: {}", loader.__class__.__name__, url, e)
 
         raise LoaderError(f"Failed to load URL: {url}")
+
+    async def async_load(self, url: str) -> str:
+        url = replace_domain(url)
+
+        for loader in self.loaders:
+            try:
+                loaded_content = await loader.async_load(url)
+
+                if not loaded_content:
+                    logger.info("[{}] Failed to load URL: {}, got empty result", loader.__class__.__name__, url)
+                    continue
+
+                logger.info("[{}] Successfully loaded URL: {}", loader.__class__.__name__, url)
+                return loaded_content
+
+            except Exception as e:
+                logger.info("[{}] Failed to load URL: {}, got error: {}", loader.__class__.__name__, url, e)
+
+        raise LoaderError(f"Failed to load URL: {url}")
--- src/kabigon/loader.py
+++ src/kabigon/loader.py
@@ -5,6 +5,9 @@ class Loader:
     def load(self, url: str) -> str:
         raise NotImplementedError
 
+    async def async_load(self, url: str) -> str:
+        raise NotImplementedError
+
 
 class LoaderError(Exception):
     pass
