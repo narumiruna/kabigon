cd9f51438db51dd7490c4a2fcae393bb2c3cf9af
narumi <toucans-cutouts0f@icloud.com>
2025-05-09 03:56:14 +0800

    ♻️ refactor(loader): remove __call__ method for clarity

--- src/kabigon/loader.py
+++ src/kabigon/loader.py
@@ -3,9 +3,6 @@ import concurrent.futures
 
 
 class Loader:
-    def __call__(self, url: str) -> str:
-        return self.load(url)
-
     def load(self, url: str) -> str:
         raise NotImplementedError
 
