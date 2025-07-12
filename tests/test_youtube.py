703c799c81a5a2050d418c8d557a4800c984c0da
narumi <toucans-cutouts0f@icloud.com>
2025-07-12 09:51:08 +0800

    delete test_youtube.py

--- tests/test_youtube.py
+++
@@ -1,9 +0,0 @@
-from kabigon.youtube import YoutubeLoader
-
-
-def test_youtube_loader() -> None:
-    url = "https://youtu.be/ZGEPXFDADm0"
-    loader = YoutubeLoader()
-    text = loader.load(url)
-    assert isinstance(text, str)
-    assert len(text) > 20
